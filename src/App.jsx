8f0fd0b5e67ac0ddefc387d4ba5bee834c4de00c
HF9110 <editor.hussein@gmail.com>
2025-11-17 23:31:48 +0300

    update34

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react'; // <-- هذا هو السطر الذي كان ناقصاً
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import AdminLogin from "./pages/AdminLogin";
