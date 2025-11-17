5175d1e84a3943ed2f94f55f60918e9fd42c4ff0
HF9110 <editor.hussein@gmail.com>
2025-11-17 22:35:07 +0300

    update35

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDUxC_2orwmSLL9iEBIkeohZKfH36MjZ4Y",
   authDomain: "ali-jabbar-week.firebaseapp.com",
   projectId: "ali-jabbar-week",
-  storageBucket: "ali-jabbar-week.firebasestorage.app",
+  storageBucket: "ali-jabbar-week.appspot.com",
   messagingSenderId: "642187294882",
   appId: "1:642187294882:web:fe30f0016e5803a5e1bffb"
 };
